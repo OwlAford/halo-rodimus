24b4a92c756ed08e00e3cbc7bb513d72c5bfd60e
OWlAford <1005476098@qq.com>
2016-03-23 14:47:13 +0800

    样式列表文件更新

--- src/assets/css/index.js
+++ src/assets/css/index.js
@@ -1,2 +1,7 @@
 require('./global.css');
 require('./swiper.css');
+require('./border.css');
+require('./datetimepicker.css');
+require('./nprogress.css');
+require('./scroller.css');
+require('./scrollLoad.css');
