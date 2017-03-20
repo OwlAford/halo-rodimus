6004eb830441f210fc3bc2fe4b662f08eee12099
Aford <244734834@qq.com>
2017-03-20 17:06:40 +0800

    first commit

--- src/assets/css/index.js
+++
@@ -1,7 +0,0 @@
-require('./global.css');
-require('./swiper.css');
-require('./border.css');
-require('./datetimepicker.css');
-require('./nprogress.css');
-require('./scroller.css');
-require('./scrollLoad.css');
